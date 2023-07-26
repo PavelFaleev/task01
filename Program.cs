6d7e1a576bad971ca388a42c468042770fd40316
PavelFaleev <fraisley2003po@gmail.com>
2023-07-26 17:44:46 +0300

    Добавлена 2 задача

--- Program.cs
+++ Program.cs
@@ -23,4 +23,38 @@ Console.WriteLine(max);
 
 
 
+                       //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+Console.Write("Здраствуйте !!! Введите перовое число для сравнения.:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("А теперь Второе число.:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("А теперь Третье число.:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+    if(a > b) max = a;
+    if(a > c) max = a;
+
+
+    if(b > a) max = b;
+    if(b > c) max = b;
+
+
+
+    if(c > a) max = c;
+    if(c > b) max = c;
+
+
+
+Console.WriteLine(max);
+
+
+
+
 
